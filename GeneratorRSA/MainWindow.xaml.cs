f785e80cd358481b87df6a27a1d4fb684e71639a
PBiskup <p.biskup@expansio.pl>
2018-11-04 12:44:15 +0100

    Add project files.

---
+++ GeneratorRSA/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+using System.Numerics;
+
+namespace GeneratorRSA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        int[] xarray;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            String ciag = Generate(2147453419, 2147460569, 1000000);
+
+            Console.WriteLine(ciag);
+
+            foreach(int x in xarray)
+            {
+                Console.WriteLine(x);
+            }
+
+        }
+
+        public String Generate(int e, int n, int lenght)
+        {
+
+            Random randomGen = new Random();
+            int random = randomGen.Next(1, n);
+
+            xarray = new int[lenght + 1];
+
+            xarray[0] = random;
+
+            String result = "";
+
+            for(int i = 0; i < lenght; i++)
+            {
+                BigInteger bigX = new BigInteger();
+                bigX = BigInteger.ModPow(new BigInteger(xarray[i]), new BigInteger(e), new BigInteger(n));
+
+                if(bigX % 2 == 0)
+                {
+                    result += "0";
+                } else
+                {
+                    result += "1";
+                }
+
+                xarray[i + 1] = (int)bigX;
+
+            }
+
+            return result;
+        }
+    }
+}
